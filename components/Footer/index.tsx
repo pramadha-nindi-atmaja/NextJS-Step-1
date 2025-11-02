9980c5072f1e91308d4a873cf098bcaf7b653bd8
pramadha <pramslabs@gmail.com>
2025-11-02 23:09:29 +0700

    Added the current year dynamically using JavaScript's Date object

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import styles from "./Footer.module.css";
 
-export default function index() {
+export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div>
-      <p className={styles.title}>info@ninedragonlabs</p>
-    </div>
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <p className={styles.copyright}>&copy; {currentYear} Nine Dragon Labs. All rights reserved.</p>
+          <div className={styles.contact}>
+            <a href="mailto:info@ninedragonlabs.com" className={styles.link}>
+              info@ninedragonlabs.com
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 }
